2710f4dda35d0cfe1fa42e7b8cfe73a583505e50
Marco Massenzio <m.massenzio@gmail.com>
2014-03-26 00:32:20 -0700

    Added persistence layer using Mongo

---
+++ src/main/scala/com/alertavert/sentinel/persistence/UserDao.scala
@@ -0,0 +1,20 @@
+package com.alertavert.sentinel.persistence
+
+import com.alertavert.sentinel.model.User
+import com.mongodb.casbah.Imports.ObjectId
+import com.alertavert.sentinel.errors.DbException
+
+/**
+ * Created by marco on 3/8/14.
+ */
+trait UserDao {
+
+  def save(user: User): ObjectId
+  def remove(userId: ObjectId): Boolean
+  def update(userId: ObjectId, user: User): Boolean
+
+  def update(user: User) = user.id match {
+    case None => throw new DbException("Cannot update a User without a valid ID")
+    case Some(x) => this.update(user.id, user)
+  }
+}
