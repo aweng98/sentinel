df3fdd913905a6da31b7ddb9d7378c580589087e
Marco Massenzio <m.massenzio@gmail.com>
2016-01-15 00:08:31 -0700

    Fixes Travis script

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -2,8 +2,8 @@
 # Copyright 2015 (c) AlertAvert.com. All rights reserved.
 # Commercial use or modification of this software without a valid license is expressly forbidden
 
-declare -r BASEDIR=$(dirname $0)/..
-declare -r TEST_CONF="${BASEDIR}/conf/tests.conf"
+declare -r BASEDIR=${HOME}/.sentinel
+declare -r TEST_CONF="${BASEDIR}/tests.conf"
 
 if [[ ! -f ${TEST_CONF} ]]; then
     echo "ERROR - ${TEST_CONF} configuration file missing."
